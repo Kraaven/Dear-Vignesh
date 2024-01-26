9bfaa37f027557a69a2817a8dbb4f170f4c58cab
Kraaven <dhruv.bhojaraj@gmail.com>
2024-01-27 02:25:16 +0530

    update 3
    -Created a Thought Display System

--- Assets/scripts/BookShelfInteraction.cs
+++ Assets/scripts/BookShelfInteraction.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class BookShelfInteraction : MonoBehaviour, IInteractableObject
 {
     public GameObject hands;
+    public TextController ThoughtUI;
     public void Interact()
     {
         Vector3 newpos = new Vector3(transform.position.x, transform.position.y, transform.position.z -1);
        GameObject hand =  Instantiate(hands,newpos,Quaternion.identity);
+       ThoughtUI.DisplayThought("Damn, thats cool",1.2f);
     }
 
     public bool ReInteract()
--- Assets/scripts/TextController.cs
+++ Assets/scripts/TextController.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TextController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public TMP_Text dialogue;
     void Start()
     {
-        
+        dialogue = gameObject.GetComponent<TMP_Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisplayThought(String message, float delay)
     {
-        
+        StartCoroutine(ShowThought(message, delay));
+    }
+
+    IEnumerator ShowThought(String message, float delay)
+    {
+        dialogue.color = new Color(1, 1, 1, 1); 
+        yield return new WaitForSeconds(delay);
+        int length = message.Length;
+        String displayedMessage = "";
+        for (int i = 0; i < length; i++)
+        {
+            displayedMessage += message[i];
+            dialogue.text = displayedMessage;
+            yield return new WaitForSeconds(Time.deltaTime * 25);
+        }
+
+        yield return new WaitForSeconds(1.75f);
+        Debug.Log("Fading");
+        for (float i = 1; i > 0 ; i-=0.05f)
+        {
+            Debug.Log(i);
+            dialogue.color = new Color(1, 1, 1, i);
+            yield return new WaitForSeconds(0.048f);
+        }
+        dialogue.text = "";
     }
 }
