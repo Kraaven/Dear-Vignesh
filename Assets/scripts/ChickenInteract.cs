68c83ebe618c0f048b16b99c7380f42977be1aaf
Asmit Singh Chauhan <blr.asmit@gmail.com>
2024-01-27 04:03:36 +0530

    Movement Updated
    -Player cannot move after picking the chicken up

--- Assets/scripts/ChickenInteract.cs
+++ Assets/scripts/ChickenInteract.cs
@@ -1,22 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ChickenInteract : MonoBehaviour , IInteractableObject
 {
     public GameObject player;
     public bool isPickedUp = false;
+    public CharacterMovement characterMovement;
+    public CameraController cameraController;
     public void Interact()
     {
         gameObject.transform.parent = player.transform;
         gameObject.transform.position = new Vector3(player.transform.position.x, 3.0f, player.transform.position.z + 1);
         gameObject.GetComponent<Collider>().enabled = false;
         isPickedUp = true;
-        // player.GetComponent<CharacterController>().enabled = false;
     }
     public bool ReInteract()
     {
         return false;
+    }
+
+    private void pickedUp()
+    {
+        if (isPickedUp == true)
+        {
+            characterMovement.speed = 0;
+        }
     }
     // Start is called before the first frame update
     void Start()
@@ -27,6 +38,6 @@ public class ChickenInteract : MonoBehaviour , IInteractableObject
     // Update is called once per frame
     void Update()
     {
-        
+        pickedUp();
     }
 }
