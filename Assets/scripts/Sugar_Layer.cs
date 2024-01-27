b9d98aa6f67e875be5b58bcf994e68f52e1006b2
Samarth Ale <127734123+Al3ias@users.noreply.github.com>
2024-01-28 01:53:24 +0530

    cake

---
+++ Assets/scripts/Sugar_Layer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sugar_Layer : MonoBehaviour,IInteractableObject
+{
+    public GameObject mould;
+    public void Interact()
+    {
+        gameObject.SetActive(false);
+        mould.transform.GetChild(3).gameObject.SetActive(true);
+    }
+
+    public bool ReInteract()
+    {
+        return false;
+    }
+}
