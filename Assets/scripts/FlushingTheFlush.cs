80dfa3e9aa14f4313f2046a4ddf2f8701edd2e93
Jai Khurana <k.jai.khurana@gmail.com>
2024-01-27 19:48:09 +0530

    Updated FlushTheFlush.cs[Section 1 WORKS]

--- Assets/scripts/FlushingTheFlush.cs
+++ Assets/scripts/FlushingTheFlush.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,30 +10,24 @@ public class FlushingTheFlush : MonoBehaviour,IInteractableObject
     public GameObject Train;
     public GameObject ui;
     public GameObject player;
-    
+    public GameObject door;
+    public GameObject wallToTrain;
     public void Interact()
     {
         if (neverSeenATrain)
         {
-            Vector3 pos = new Vector3(transform.position.x +30, transform.position.y , transform.position.z);
+            Vector3 pos = new Vector3(transform.position.x +30, transform.position.y , transform.position.z - 2.15f);
             GameObject train = Instantiate(Train, pos, Quaternion.identity); // spawn train
+            door.SetActive(true);
+            wallToTrain.SetActive(false);
+            neverSeenATrain = false;
             StartCoroutine(moveTrain(train)); // Train animation
-            if (train.transform.position.x <= -3f)
-            {
-                ui.SetActive(true);
-                player.transform.position = new Vector3(0.22f, 1.85f, 1.24f);
-                Invoke("openEyes", 2);
-                Debug.Log("GAME OVER!");
-                neverSeenATrain = false;
-            }
         }
         else
         {
-            //TODO:add this
             Debug.Log("ye bharna h abhi");
         }
     }
-
     public void openEyes()
     {
         ui.SetActive(false);
@@ -45,11 +40,17 @@ public class FlushingTheFlush : MonoBehaviour,IInteractableObject
 
     IEnumerator moveTrain(GameObject T)
     {
-        for (int i = 0; i < 350; i++)
+        for (int i = 0; i < 300; i++)
         {
             T.transform.Translate(-0.1f, 0, 0);
-            Debug.Log(T.transform.position.x);
             yield return new WaitForSeconds(Time.deltaTime);
         }
+        Debug.Log("GAME OVER!");
+        ui.SetActive(true);
+        player.transform.position = new Vector3(0f, 1.85f, 0f);
+        Invoke("openEyes", 2);
+        Destroy(T);
+        wallToTrain.SetActive(true);
+        door.SetActive(false); 
     }
 }
