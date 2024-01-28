01b1f19905567e67145313f63b5e949618343bac
Kraaven <dhruv.bhojaraj@gmail.com>
2024-01-28 10:47:20 +0530

    notepad
    -Added Notepad
    -Notepad can be modified using the scripts

--- Assets/scripts/PlayerSit.cs
+++ Assets/scripts/PlayerSit.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerSit : MonoBehaviour
 {
     public GameObject Sofa;
+
+    public NotepadController check;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,9 @@ public class PlayerSit : MonoBehaviour
         gameObject.GetComponent<Collider>().enabled = false;
         gameObject.GetComponent<Rigidbody>().useGravity = false;
         transform.Translate(-0.3f,0,0);
+        
+        check.CutEgg();
+        check.ConvertStrawberry();
     }
 
     // Update is called once per frame
